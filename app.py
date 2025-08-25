faebfcf00580fcddaef72b535a286a9f2d255eb3
illbetheree <niranjan.ece2022@jerusalemengg.ac.in>
2025-08-25 11:08:26 +0530

    app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ with st.spinner("Loading assets and calculating base vulnerability..."):
         model, historical_data = load_model_and_history()
     else:
         st.error("Could not generate the base Flood Vulnerability Index. The application cannot proceed."); st.stop()
-st.info("Newer Gen Model is on the developement")
+st.info("Newer Gen Model is on the developement, Will be released soon...")
 st.sidebar.header("Dashboard Options")
 view_selection = st.sidebar.radio("Select View:", ("Current Risk", "24-Hour Forecasted Risk"))
 st.sidebar.header("Risk Simulator")
@@ -310,3 +310,4 @@ if base_fvi_gdf is not None and model is not None:
             st.subheader("24-Hour Rainfall Forecast Visualization")
 
             st.bar_chart(forecast_df.set_index('Timestamp')['predicted_rainfall'])
+
